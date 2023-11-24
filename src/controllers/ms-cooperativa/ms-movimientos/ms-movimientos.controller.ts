7843c85df2189ac090faf8ca10c55b63c4015b79
Diego Martinez <martinezd@presidencia.gob.ec>
2023-11-24 00:31:20 -0500

    MS-COOP / Registro transacción usuario.

--- src/controllers/ms-cooperativa/ms-movimientos/ms-movimientos.controller.ts
+++ src/controllers/ms-cooperativa/ms-movimientos/ms-movimientos.controller.ts
@@ -6,10 +6,27 @@ import {
   Patch,
   Param,
   Delete,
+  Res,
+  Req,
+  UploadedFiles,
+  UseGuards,
+  UsePipes,
+  UseInterceptors,
+  HttpStatus,
 } from '@nestjs/common';
+import { Response } from 'express';
+
+import { multerImagen } from '@app/libs/multer/multer.imagen';
+
+import { Autorizacion } from '@decorators/autorizacion.decorator';
+
+import { SeguridadGuard } from '@guards/seguridad.guard';
 
 import { CreateMsMovimientoDto } from '@models/ms-cooperativa/ms-movimientos/dto/create-ms-movimiento.dto';
 import { UpdateMsMovimientoDto } from '@models/ms-cooperativa/ms-movimientos/dto/update-ms-movimiento.dto';
+import { AutorizacionUsuarioDto } from '@models/ms-seguridad/ms-usuarios/dto/autorizacion-usuario.dto';
+
+import { BodyParamsPipe } from '@pipes/bodyparams/bodyparams.pipe';
 
 import { MsMovimientosService } from '@services/ms-cooperativa/ms-movimientos/ms-movimientos.service';
 
@@ -17,9 +34,35 @@ import { MsMovimientosService } from '@services/ms-cooperativa/ms-movimientos/ms
 export class MsMovimientosController {
   constructor(private readonly msMovimientosService: MsMovimientosService) {}
 
+  @UseGuards(SeguridadGuard)
+  @UsePipes(new BodyParamsPipe())
+  @UseInterceptors(multerImagen)
   @Post()
-  create(@Body() createMsMovimientoDto: CreateMsMovimientoDto) {
-    return this.msMovimientosService.create(createMsMovimientoDto);
+  create(
+    @Body() createMsMovimientoDto: CreateMsMovimientoDto,
+    @Res() response: Response,
+    @Req() request: Request,
+    @UploadedFiles() files: Array<Express.Multer.File>,
+    @Autorizacion() autorizacionUsuarioDto: AutorizacionUsuarioDto,    
+  ) {
+    try {      
+      // * recogemos la url...
+      const serverUrl = request['serverurl'];
+      // * crea el movimiento...
+      this.msMovimientosService
+      .create(createMsMovimientoDto, files, serverUrl, autorizacionUsuarioDto)
+      .subscribe({
+        next(movimiento) {
+          // * responde el token...
+          return response.status(HttpStatus.OK).json(movimiento);
+        },
+        error(err) {
+          return response.status(HttpStatus.BAD_REQUEST).json(err);
+        },        
+      });
+    } catch (error) {
+      throw error;
+    }
   }
 
   @Get()
--- src/modules/ms-cooperativa/ms-movimientos/ms-movimientos.module.ts
+++ src/modules/ms-cooperativa/ms-movimientos/ms-movimientos.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 import { MsMovimientosController } from '@controllers/ms-cooperativa/ms-movimientos/ms-movimientos.controller';
+
 import { MsMovimientosService } from '@services/ms-cooperativa/ms-movimientos/ms-movimientos.service';
+import { MsAutorizacionService } from '@services/ms-seguridad/ms-autorizacion.service';
+
+import config from '@app/libs/config/config';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: config().microservicios.cooperativa.alias,
+        transport: Transport.TCP,
+        options: {
+          host: config().microservicios.cooperativa.nombre,
+          port: config().microservicios.cooperativa.puerto,
+        },
+      },
+    ]),
+    ConfigModule,
+  ],  
   controllers: [MsMovimientosController],
-  providers: [MsMovimientosService],
+  providers: [MsMovimientosService, MsAutorizacionService],
 })
 export class MsMovimientosModule {}
